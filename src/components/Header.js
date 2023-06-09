ea82fa02dadbfe94b30d978bde4f8992287ee1c0
Bisaso Ronald <bronnie313@gmail.com>
2023-06-09 06:22:59 +0300

    Add linter and header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import NavLink from 'react-router-dom';
+
+const Header = () => (
+  <nav>
+    <div className="logo">Bookstore</div>
+    <ul className="navlinks">
+      <li><NavLink className="link" to="/">Book</NavLink></li>
+      <li><NavLink className="link" to="/Categories">Categories</NavLink></li>
+    </ul>
+  </nav>
+);
+
+export default Header;
