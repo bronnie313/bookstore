1ca209a2b2be658b831dec7bf9b984507d15a258
Bisaso Ronald <bronnie313@gmail.com>
2023-06-10 18:27:37 +0300

    add data to the books component

--- src/components/Books.js
+++ src/components/Books.js
@@ -35,9 +35,35 @@ const Book = (props) => {
   const { categories, title, author } = props;
   return (
     <article className="book">
-      <h3>{categories}</h3>
-      <h2>{title}</h2>
-      <h6>{author}</h6>
+      <div className="book_item">
+        <div>
+          <h3>{categories}</h3>
+          <h2>{title}</h2>
+          <h6>{author}</h6>
+          <div className="book_btns">
+            <p>Comment</p>
+            <line className="vl" />
+            <p>Remove</p>
+            <line className="vl" />
+            <p>Edit</p>
+          </div>
+        </div>
+        <div className="books_updates">
+          <div className="progress">
+            <img src="./progress.png" alt="progress" />
+            <div className="completed">
+              <p className="percent">8%</p>
+              <p>completed</p>
+            </div>
+          </div>
+          <vline className="vline" />
+          <div className="updates">
+            <p>Current Chapter</p>
+            <p>Chapter 12</p>
+            <button type="button"> UPDATE PROGRESS</button>
+          </div>
+        </div>
+      </div>
     </article>
   );
 };
