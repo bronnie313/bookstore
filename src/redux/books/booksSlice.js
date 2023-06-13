796f07a636a63445350293c9e507def5b18dc900
Bisaso Ronald <bronnie313@gmail.com>
2023-06-13 14:04:17 +0300

    add addBook and removeBook reducers

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -9,8 +9,17 @@ const booksSlice = createSlice({
   name: 'book',
   initialState,
   reducers: {
-
+    addBook: (state, action) => {
+        const newBook = action.payload;
+        state.book.push(newBook);
+    },
+    removeBook: (state, action) => {
+        const bookId = action.payload;
+        state.bookItems = state.bookItems.filter((item) => item.id !== bookId)
+    }
   },
 });
+
+export const { addBook, removeBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
